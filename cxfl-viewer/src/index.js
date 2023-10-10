56118d8e2e6ca567bf095d0c917849f9c9b955db
ExodexoDev <103096955+ExodexoDev@users.noreply.github.com>
2023-10-10 14:45:58 -0500

    CSS Modularization, some slight experimentation

--- cxfl-viewer/src/index.js
+++ cxfl-viewer/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './main.js';
 import reportWebVitals from './reportWebVitals';
-import './globalStyles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
