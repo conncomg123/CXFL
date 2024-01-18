29eaa17a3a445b2e6259b5370775d06935601926
123connorpoop <rct3fan55@yahoo.com>
2024-01-18 09:57:09 -0500

    SymbolItem hotfix

--- CsXFL/SymbolItem.cs
+++ CsXFL/SymbolItem.cs
@@ -3,10 +3,22 @@ using System.Xml.Linq;
 namespace CsXFL;
 public class SymbolItem : Item
 {
+    public static class DefaultValues
+    {
+        public const string SymbolType = "movie clip";
+    }
     private static readonly List<string> AcceptableSymbolTypes = new List<string> { "graphic", "button", "movie clip" };
     private readonly string symbolType;
     private readonly Timeline timeline;
-    public string SymbolType { get { return symbolType; } }
+    public string SymbolType
+    {
+        get { return symbolType; }
+        set
+        {
+            if (!AcceptableSymbolTypes.Contains(value)) throw new ArgumentException("Invalid symbol type: " + value);
+            Root?.SetOrRemoveAttribute("symbolType", value, DefaultValues.SymbolType);
+        }
+    }
     public Timeline Timeline { get { return timeline; } }
     public SymbolItem() : base()
     {
@@ -14,13 +26,13 @@ public class SymbolItem : Item
         symbolType = string.Empty;
         timeline = new Timeline();
     }
-    public SymbolItem(in XElement symbolItemNode) : base(symbolItemNode, (string)symbolItemNode.Attribute("symbolType")!)
+    public SymbolItem(in XElement symbolItemNode) : base(symbolItemNode, (string?)symbolItemNode.Attribute("symbolType") ?? DefaultValues.SymbolType)
     {
-        if (!AcceptableSymbolTypes.Contains((string)symbolItemNode.Attribute("symbolType")!))
+        if (!AcceptableSymbolTypes.Contains((string?)symbolItemNode.Attribute("symbolType") ?? DefaultValues.SymbolType))
         {
             throw new ArgumentException("Invalid symbol type: " + (string)symbolItemNode.Attribute("symbolType")!);
         }
-        symbolType = (string)symbolItemNode.Attribute("symbolType")!;
+        symbolType = (string?)symbolItemNode.Attribute("symbolType") ?? DefaultValues.SymbolType;
         timeline = new Timeline(symbolItemNode.Element(ns + "timeline")!.Element(ns + "DOMTimeline")!);
     }
     public SymbolItem(in SymbolItem other) : base(other)
