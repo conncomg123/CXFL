54f02bca0d71f6e4fb7569198fa0c569b7ba15dd
123connorpoop <rct3fan55@yahoo.com>
2024-01-27 19:48:03 -0500

    Publicize SoundUtils

--- CsXFL/SoundUtils.cs
+++ CsXFL/SoundUtils.cs
@@ -1,10 +1,11 @@
 using NAudio.Wave;
 using NAudio.Flac;
-internal class SoundUtils
+namespace CsXFL;
+public class SoundUtils
 {
-    internal static byte[] ConvertFlacToWav(string flacFilePath)
+    public static byte[] ConvertFlacToWav(string flacFilePath)
     {
-        var memStream = new MemoryStream();
+        using var memStream = new MemoryStream();
         using var reader = new FlacReader(flacFilePath);
         using var writer = new WaveFileWriter(memStream, reader.WaveFormat);
         Span<byte> buffer = new byte[8192];
@@ -17,16 +18,21 @@ internal class SoundUtils
         }
         return memStream.ToArray();
     }
-    internal static string GetSoundFormat(string soundFilePath)
+    public static string GetSoundFormat(string soundFilePath)
     {
         using var file = TagLib.File.Create(soundFilePath);
         string format = $"{file.Properties.AudioSampleRate / 1000}kHz {file.Properties.BitsPerSample}bit {(file.Properties.AudioChannels == 1 ? "Mono" : "Stereo")}";
         return format;
     }
-    internal static int GetSoundSampleCount(string soundFilePath)
+    public static int GetSoundSampleCount(string soundFilePath)
     {
         using var file = TagLib.File.Create(soundFilePath);
         int sampleCount = (int) Math.Round(file.Properties.AudioSampleRate * file.Properties.Duration.TotalSeconds);
         return sampleCount;
+    }
+    public static double GetSoundDuration(string soundFilePath)
+    {
+        using var file = TagLib.File.Create(soundFilePath);
+        return file.Properties.Duration.TotalSeconds;
     }
 }
