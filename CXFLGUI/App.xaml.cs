db0c00fd3ee225c03a897d30bddc76e349b6da7e
123connorpoop <rct3fan55@yahoo.com>
2024-02-11 16:01:16 -0500

    Boilerplate for MAUI

---
+++ CXFLGUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CXFLGUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CXFLGUI/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace CXFLGUI
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        CsXFL.Document doc;
+        public MainPage()
+        {
+            doc = new("C:\\Stuff\\CXFL\\CsXFL.Tests\\TestAssets\\test.fla");
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            CounterBtn.Text = doc.GetTimeline(0).GetFrameCount().ToString();
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
