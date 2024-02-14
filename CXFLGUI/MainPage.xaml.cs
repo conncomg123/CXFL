d9837f9d7a3039f276d0de683ae176b55751bccc
ExodexoDev <103096955+ExodexoDev@users.noreply.github.com>
2024-02-13 23:05:22 -0600

    Primitive Resizable Frame Construct
    Kill me

--- CXFLGUI/MainPage.xaml.cs
+++ CXFLGUI/MainPage.xaml.cs
@@ -1,28 +1,11 @@
-﻿using CommunityToolkit.Maui;
-using Microsoft.Extensions.Logging;
-using System.Diagnostics;
-
-namespace CXFLGUI
+﻿namespace CXFLGUI
 {
-    public static class MauiProgram
+    public partial class MainPage : ContentPage
     {
-        public static MauiApp CreateMauiApp()
+        public MainPage()
         {
-            var builder = MauiApp.CreateBuilder();
-            builder.Logging.AddDebug();
-            builder
-                .UseMauiApp<App>()
-                // Initialize the .NET MAUI Community Toolkit by adding the below line of code
-                .UseMauiCommunityToolkit()
-                // After initializing the .NET MAUI Community Toolkit, optionally add additional fonts
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
-
-            // Continue initializing your .NET MAUI App here
-            return builder.Build();
+            InitializeComponent();
+            BindingContext = new MainViewModel();
         }
     }
 }
--- CXFLGUI/Resources/Containers/VanillaFrame.cs
+++ CXFLGUI/Resources/Containers/VanillaFrame.cs
@@ -17,10 +17,12 @@ namespace CXFLGUI
         double InitialWidth = 300;
         double InitialHeight = 250;
 
+        int ResizeTolerance = 15;
+
         public VanillaFrame()
         {
-            Background = Colors.DarkGray;
-            BorderColor = Colors.Grey;
+            Background = (Color)App.Fixed_ResourceDictionary["Colors"]["Primary"];
+            BorderColor = (Color)App.Fixed_ResourceDictionary["Colors"]["Gray500"];
             CornerRadius = 5;
             HasShadow = true;
             Padding = 1;
@@ -34,33 +36,70 @@ namespace CXFLGUI
             grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
             grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
 
-            var blackBox = new BoxView { BackgroundColor = Colors.Black };
-            grid.Children.Add(blackBox);
-            Grid.SetRow(blackBox, 0);
-            Grid.SetColumnSpan(blackBox, 2);
+            var topBrush = new BoxView { Color = (Color)App.Fixed_ResourceDictionary["Colors"]["PrimaryDark"] };
+            grid.Children.Add(topBrush);
+            Grid.SetRow(topBrush, 0);
+            Grid.SetColumnSpan(topBrush, 2);
 
             Content = grid;
 
+            var panGesture = new PanGestureRecognizer();
+            panGesture.PanUpdated += OnPanUpdated;
+            this.GestureRecognizers.Add(panGesture);
+
         }
+
+        private void OnPanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            switch (e.StatusType)
+            {
+                case GestureStatus.Started:
+                    if (IsPointerOverBorder(e.TotalX, e.TotalY))
+                    {
+                        isResizing = true;
+                        resizeStartPosition = new Point(e.TotalX, e.TotalY);
+                    }
+                    break;
+                case GestureStatus.Running:
+                    if (isResizing && resizeStartPosition != null)
+                    {
+                        var deltaX = e.TotalX - resizeStartPosition.Value.X;
+                        var deltaY = e.TotalY - resizeStartPosition.Value.Y;
+
+                        WidthRequest = InitialWidth + deltaX;
+                        HeightRequest = InitialHeight + deltaY;
+                    }
+                    break;
+                case GestureStatus.Completed:
+                case GestureStatus.Canceled:
+                    isResizing = false;
+                    break;
+            }
+        }
+
         private void VanillaFrame_PointerEntered(object sender, PointerEventArgs e)
         {
-            //Window.Current.CoreWindow.PointerCursor = new CoreCursor(CoreCursorType.SizeNorthwestSoutheast, 1);
+            if (sender is VisualElement visualElement)
+            {
+                CursorBehavior.SetCursor(visualElement, CursorIcon.Cross);
+            }
         }
 
         private void VanillaFrame_PointerExited(object sender, PointerEventArgs e)
         {
-            if (!isResizing)
+            if (sender is VisualElement visualElement)
             {
-                //Window.Current.CoreWindow.PointerCursor = new CoreCursor(CoreCursorType.Arrow, 1);
+                CursorBehavior.SetCursor(visualElement, CursorIcon.Hand);
             }
         }
 
         private void VanillaFrame_PointerPressed(object sender, PointerEventArgs e)
         {
-            if (IsPointerOverBorder(e))
+            var position = e.GetPosition(this);
+            if (IsPointerOverBorder((double)(position?.X), (double)(position?.Y)))
             {
                 isResizing = true;
-                resizeStartPosition = e.GetPosition(this);
+                resizeStartPosition = position;
             }
         }
 
@@ -71,7 +110,7 @@ namespace CXFLGUI
 
         private void VanillaFrame_PointerMoved(object sender, PointerEventArgs e)
         {
-            if (isResizing)
+            if (isResizing && resizeStartPosition != null)
             {
                 var currentPosition = e.GetPosition(this);
                 var deltaX = currentPosition?.X - resizeStartPosition?.X;
@@ -82,10 +121,9 @@ namespace CXFLGUI
             }
         }
 
-        private bool IsPointerOverBorder(PointerEventArgs e)
+        private bool IsPointerOverBorder(double x, double y)
         {
-            var pointerPosition = e.GetPosition(this);
-            return pointerPosition?.X < 5 || pointerPosition?.Y < 5 || pointerPosition?.X > Width - 5 || pointerPosition?.Y > Height - 5;
+            return x < ResizeTolerance || y < ResizeTolerance || x > Width - ResizeTolerance || y > Height - ResizeTolerance;
         }
     }
 }
