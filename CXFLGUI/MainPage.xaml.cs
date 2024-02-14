49f045d9f28db8c3675249443110257015ecdb2e
123connorpoop <rct3fan55@yahoo.com>
2024-02-13 22:29:32 -0500

    ViewModel beginnings

--- CXFLGUI/MainPage.xaml.cs
+++ CXFLGUI/MainPage.xaml.cs
@@ -2,19 +2,10 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-        CsXFL.Document doc;
         public MainPage()
         {
-            doc = new("C:\\Users\\Administrator\\Elements of Justice\\303_Autogen_FLAs\\303_S1.fla");
             InitializeComponent();
+            BindingContext = new MainViewModel();
         }
-
-        //private void OnCounterClicked(object sender, EventArgs e)
-        //{
-        //    CounterBtn.Text = doc.GetTimeline(0).GetFrameCount().ToString();
-
-        //    SemanticScreenReader.Announce(CounterBtn.Text);
-        //}
     }
 }
