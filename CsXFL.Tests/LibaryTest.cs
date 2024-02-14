7dc95a12ec6e0207ea3f2c7cef020e4956d05541
jmc718 <50588263+jmc718@users.noreply.github.com>
2024-02-14 13:59:17 -0700

    Add Test for Library.RemoveItem

--- CsXFL.Tests/LibaryTest.cs
+++ CsXFL.Tests/LibaryTest.cs
@@ -16,4 +16,18 @@ public class LibraryTest
         Assert.True(doc.Library.ItemExists(newName));
         Assert.False(doc.Library.ItemExists(itemName));
     }
+    
+    [Fact]
+    public void RemoveItem_ShouldRemoveItem()
+    {
+        // Arrange
+        Document doc = new("TestAssets/DOMDocument.xml");
+        string itemName = "Loop.wav";
+
+        // Act
+        doc.Library.RemoveItem(itemName);
+
+        // Assert
+        Assert.False(doc.Library.ItemExists(itemName));
+    }
 }
