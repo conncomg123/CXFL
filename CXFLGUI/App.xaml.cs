3c4e83fb318d2a9ffabe07cab7a3bd2edd9e4f52
ExodexoDev <103096955+ExodexoDev@users.noreply.github.com>
2024-02-13 23:05:22 -0600

    Primitive Resizable Frame Construct
    Kill me

--- CXFLGUI/App.xaml.cs
+++ CXFLGUI/App.xaml.cs
@@ -2,11 +2,19 @@
 {
     public partial class App : Application
     {
+        public static Dictionary<string, ResourceDictionary> Fixed_ResourceDictionary { get; private set; }
+
         public App()
         {
             InitializeComponent();
-
             MainPage = new AppShell();
+
+            Fixed_ResourceDictionary = new Dictionary<string, ResourceDictionary>();
+            foreach (var dictionary in Application.Current.Resources.MergedDictionaries)
+            {
+                string key = dictionary.Source.OriginalString.Split(';').First().Split('/').Last().Split('.').First();
+                Fixed_ResourceDictionary.Add(key, dictionary);
+            }
         }
     }
 }
