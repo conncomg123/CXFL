bccbc66a97fe2655974edc450d2d040128fd93e9
ExodexoDev <103096955+ExodexoDev@users.noreply.github.com>
2024-02-22 21:20:36 -0600

    BlinkAPI - Needs fine-tuning + Stopwatch Scene Gen support
    Very fast

---
+++ SceneGenerator/BlinkAPI.cs
@@ -0,0 +1,59 @@
+﻿using CsXFL;
+using System.Drawing.Printing;
+using System.Security.Cryptography.X509Certificates;
+namespace SceneGenerator.API;
+
+static class BlinkAPI
+{
+    private static int BLINK_DURATION = 6;
+
+    private static double GammaVariable(double mean, int k)
+    {
+        double theta = mean / k;
+        double sum = 0;
+        Random random = new Random();
+
+        for (int i = 0; i < k; i++)
+        {
+            sum -= Math.Log(random.NextDouble()) * theta;
+        }
+
+        return sum;
+    }
+
+    public static void AutomaticBlinking(this Document Doc, double mean)
+    {
+        int k = 25;
+        double FPS = Doc.FrameRate;
+
+        for (int SceneIndex = 0; SceneIndex < Doc.Timelines.Count; SceneIndex++)
+        {
+            Doc.CurrentTimeline = SceneIndex;
+            Timeline CurrentTimeline = Doc.GetTimeline(Doc.CurrentTimeline);
+
+            foreach (Layer CurrentLayer in CurrentTimeline.Layers)
+            {
+                if (CurrentLayer.ParentLayerIndex is null) continue;
+                if (CurrentTimeline.Layers[CurrentLayer.ParentLayerIndex.Value].Name == "VECTOR_CHARACTERS")
+                {
+                    for (int FrameIndex = 0; FrameIndex < CurrentLayer.GetFrameCount();)
+                    {
+                        int Stare = (int)(GammaVariable(mean, k) * FPS) + FrameIndex;
+                        // Terrible job, supershit!
+                        if (Stare >= CurrentLayer.GetFrameCount()) { break; }
+                        Frame ConsiderFrame = CurrentLayer.GetFrame(Stare);
+
+                        if (!ConsiderFrame.IsEmpty())
+                        {
+                            CurrentLayer.ConvertToKeyframes(Stare);
+                            CurrentLayer.GetFrame(Stare).Name = "Blink";
+                            CurrentLayer.GetFrame(Stare).LabelType = "anchor";
+                        }
+
+                        FrameIndex += Stare;
+                    }
+                }
+            }
+        }
+    }
+}
