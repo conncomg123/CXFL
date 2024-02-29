50fe212e99ff83ca6014142d3443af1735adf7ae
ExodexoDev <103096955+ExodexoDev@users.noreply.github.com>
2024-02-29 15:52:36 -0600

    Safety push pt 2

--- XFLtoSESX/XFL2SESX.cs
+++ XFLtoSESX/XFL2SESX.cs
@@ -4,21 +4,21 @@ using System.Xml;
 using System.Xml.Linq;
 using System.Collections.Generic;
 using CsXFL;
+using CSCore;
+using CSCore.Codecs;
+using CSCore.Codecs.FLAC;
+using CSCore.SoundOut;
+using CSCore.Streams;
+using CSCore.Streams.Effects;
 using static XFL2SESX.XFL2SESX;
+using System.Diagnostics;
+using CSCore.Codecs.AAC;
+using System.Text;
 
 namespace XFL2SESX
 {
     static class XFL2SESX
     {
-        static int CorrectStartSample(int startSample, int sampleRate = 48000, double desyncMilliseconds = 0.0)
-        {
-            int desyncSamples = (int)Math.Round(desyncMilliseconds * sampleRate / 1000.0);
-
-            int correctedStartSample = startSample + desyncSamples;
-
-            return correctedStartSample;
-        }
-
         static void AddAudioData(Dictionary<string, List<AudioClip>> audioData, Dictionary<string, int> fileIds, string layerName, string audioFileName, int startSample, int endSample, int sourceInPoint, int sourceOutPoint)
         {
             if (!audioData.ContainsKey(layerName))
@@ -69,6 +69,17 @@ namespace XFL2SESX
             return startSample;
         }
 
+        static string GetAfterLastSlash(string input)
+        {
+            int lastIndex = input.LastIndexOf('/');
+            if (lastIndex == -1)
+            {
+                return input;
+            }
+
+            return input.Substring(lastIndex + 1);
+        }
+
         public class AudioClip
         {
             public int id { get; set; }
@@ -81,53 +92,117 @@ namespace XFL2SESX
             public int zOrder { get; set; }
         }
 
-        static void Main()
+        static void Main(string[] args)
         {
+            List<string> FLAfileList = new List<string>();
+            string FLAfilePaths_ARGS = "";
+            string audioFolderPath_ARGS = "";
+            bool displayHelp = false;
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                string arg = args[i];
+
+                if (arg == "--help" || arg == "-h" || arg == "-?")
+                {
+                    displayHelp = true;
+                }
+                else if (i == 0)
+                {
+                    // Process FLAs
+                    FLAfilePaths_ARGS = arg;
+                    string[] files = arg.Split('|');
+                    foreach (var file in files)
+                    {
+                        FLAfileList.Add(file);
+                    }
+                }
+                else if (i == 1)
+                {
+                    // Process Audio Folder
+                    audioFolderPath_ARGS = arg;
+                }
+            }
+
+            if (displayHelp || string.IsNullOrEmpty(FLAfilePaths_ARGS) || string.IsNullOrEmpty(audioFolderPath_ARGS))
+            {
+                Console.WriteLine("Usage:");
+                Console.WriteLine("    XFL2SESX.exe [AbsolutePathToFLA|FLAList] AbsolutePathToAudioFolder");
+                Console.WriteLine("Options:");
+                Console.WriteLine("    --help, -h   Display this help message");
+                Console.WriteLine("Description:");
+                Console.WriteLine("    An Adobe Animate to Adobe Audition file converter to assist with mastering projects made in Animate.");
+                Console.WriteLine("This utility supports single file operation on an FLA, or batch operation across multiple FLAs, yielding");
+                Console.WriteLine("one compiled SESX file. When the SESX is compiled, dependent audio files will need to be placed in the");
+                Console.WriteLine("'Imported Files' folder where the SESX is located. The required AbsolutePathToAudioFolder will convert all");
+                Console.WriteLine("audio files in that folder to a compatible format, and automatically move them into this folder. Audio files");
+                Console.WriteLine("not present in this audio folder but required by the SESX will need to placed into the folder manually.");
+                Console.WriteLine("");
+                Console.WriteLine("To process multiple FLAs, provide the paths inside quotes, delimited by the vertical pipe character.");
+                Console.WriteLine("    (ex.) XFL2SESX.exe \"C:\\File\\File01.FLA|C:\\File\\File02.FLA|C:\\File\\File03.FLA\" \"C:\\AudioFolder\"");
+                Console.WriteLine("");
+                return;
+            }
+
             Dictionary<string, int> fileIds = new Dictionary<string, int>();
             Dictionary<string, List<AudioClip>> AUDIO_DATA = new Dictionary<string, List<AudioClip>>();
             int SampleRate = 48000;
 
-            string inputFile = @"C:\Users\Administrator\Elements of Justice\303_Autogen_FLAs\RealTestNoSplitAudio.fla";
-            string fileName = Path.GetFileNameWithoutExtension(inputFile);
-            string directory = Path.GetDirectoryName(inputFile);
+            string primaryFile = "";
 
-            string directory_SESX = directory + @"\Sesx Conversion\Imported Files";
-            directory_SESX = directory + @"\" + fileName + @" Sesx Conversion\";
+            foreach (string file in FLAfileList)
+            {
+                primaryFile = file;
+                break;
+            }
+
+            string fileName = Path.GetFileNameWithoutExtension(primaryFile);
+            string directory = Path.GetDirectoryName(primaryFile);
+
+            string directory_SESX = directory + @"\" + fileName + @" Sesx Conversion\Imported Files";
             if (!Directory.Exists(directory_SESX)) { Directory.CreateDirectory(directory_SESX); }
+            directory_SESX = directory + @"\" + fileName + @" Sesx Conversion\";
 
             XmlDocument SESX = new XmlDocument();
-            Document Doc = new(inputFile);
 
             // Extract Audio Data
-            int totalFrames = 0;
-            for (int SceneIndex = 0; SceneIndex < Doc.Timelines.Count; SceneIndex++)
+            int totalFrames = 2;
+            foreach (string file in FLAfileList)
             {
-                if (SceneIndex > 0) { totalFrames += Doc.Timelines[SceneIndex - 1].GetFrameCount(); }
-                foreach (var OperatingLayer in Doc.Timelines[SceneIndex].Layers)
+                Document Doc = new(file);
+                totalFrames -= 2;
+                for (int SceneIndex = 0; SceneIndex < Doc.Timelines.Count; SceneIndex++)
                 {
-                    if (OperatingLayer.Name.Contains("VOX") || OperatingLayer.Name.Contains("SFX"))
+                    foreach (var OperatingLayer in Doc.Timelines[SceneIndex].Layers)
                     {
-                        foreach (var OperatingFrame in OperatingLayer.KeyFrames)
+                        if (OperatingLayer.Name.Contains("VOX", StringComparison.OrdinalIgnoreCase) || OperatingLayer.Name.Contains("SFX", StringComparison.OrdinalIgnoreCase))
                         {
-                            if (OperatingFrame.SoundName == "") { continue; }
-                            string SoundName = OperatingFrame.SoundName;
-                            SoundItem AudioFile = Doc.Library.Items[SoundName] as SoundItem;
-                            int SampleCount = AudioFile.SampleCount;
-                            int startSample = FrameToSamples(Doc, SampleRate, OperatingFrame.StartFrame + totalFrames);
-                            int endSample = startSample + SampleCount;
-
-                            int SplitAudioOffset = OperatingFrame.InPoint44;
-
-                            if (SplitAudioOffset == 0)
+                            foreach (var OperatingFrame in OperatingLayer.KeyFrames)
                             {
-                                AddAudioData(AUDIO_DATA, fileIds, OperatingLayer.Name, SoundName, startSample, endSample, 0, SampleCount + 48000);
-                            } else
-                            {
-                                SplitAudioOffset = ConvertSamples(SplitAudioOffset, 44000, SampleRate);
-                                AddAudioData(AUDIO_DATA, fileIds, OperatingLayer.Name, SoundName, startSample, endSample, SplitAudioOffset, (SampleCount - SplitAudioOffset) + 48000);
+                                if (OperatingFrame.SoundName == "") { continue; }
+                                string SoundName = OperatingFrame.SoundName;
+                                SoundItem AudioFile = Doc.Library.Items[SoundName] as SoundItem;
+
+                                // Either get the full the sample count of the file, or get what section of it would be displayed, if it is interrupted by a blank keyframe. Whichever first.
+                                int SampleCount = Math.Min(AudioFile.SampleCount, FrameToSamples(Doc, SampleRate, OperatingFrame.Duration));
+                                int startSample = FrameToSamples(Doc, SampleRate, OperatingFrame.StartFrame + totalFrames);
+                                int endSample = startSample + SampleCount;
+
+                                int SplitAudioOffset = OperatingFrame.InPoint44;
+
+                                if (SplitAudioOffset == 0)
+                                {
+                                    AddAudioData(AUDIO_DATA, fileIds, OperatingLayer.Name.Replace("_VOX", "").Replace("_1", ""), GetAfterLastSlash(SoundName), startSample, endSample, 0, SampleCount + (SampleRate / 2));
+                                }
+                                else
+                                {
+                                    SplitAudioOffset = ConvertSamples(SplitAudioOffset, 44000, SampleRate);
+                                    AddAudioData(AUDIO_DATA, fileIds, OperatingLayer.Name.Replace("_VOX", "").Replace("_1", ""), GetAfterLastSlash(SoundName), startSample, endSample, SplitAudioOffset, SampleCount + (SampleRate/2));
+                                }
                             }
                         }
                     }
+                    totalFrames += Doc.Timelines[SceneIndex].GetFrameCount();
                 }
             }
 
@@ -234,6 +309,7 @@ namespace XFL2SESX
                     audioElement.SetAttribute("sourceInPoint", audioClip.sourceInPoint.ToString());
                     audioElement.SetAttribute("endPoint", audioClip.endPoint.ToString());
                     audioElement.SetAttribute("sourceOutPoint", audioClip.sourceOutPoint.ToString());
+                    audioElement.SetAttribute("clipAutoCrossfade", "false");
                     audioElement.SetAttribute("zOrder", audioClip.zOrder.ToString());
 
                     audioTrackElement.AppendChild(audioElement);
@@ -244,8 +320,47 @@ namespace XFL2SESX
 
             root.AppendChild(filesElement);
 
-            string outputFile = Path.Combine(directory_SESX, Path.GetFileNameWithoutExtension(Doc.Filename) + ".sesx");
-            Console.WriteLine(outputFile);
+            // Automatically port over provided files
+            string outputDirectory = directory + @"\" + fileName + @" Sesx Conversion\Imported Files";
+            string[] audioFiles = Directory.GetFiles(audioFolderPath_ARGS, "*.*", SearchOption.AllDirectories);
+            foreach (string audioFile in audioFiles)
+            {
+                string inputAudioFile = audioFile;
+                string outputAudioFile = outputDirectory + "\\" + Path.GetFileName(audioFile);
+
+                string ffmpegCommand = $"-i \"{inputAudioFile}\" -c:a flac -ar {SampleRate} -ac 1 -sample_fmt s32 \"{outputAudioFile}\"";
+
+                Process process = new Process();
+                string ffmpegPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ffmpeg.exe");
+                process.StartInfo.FileName = ffmpegPath;
+                process.StartInfo.Arguments = ffmpegCommand;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.RedirectStandardError = true;
+
+                process.Start();
+
+                StringBuilder error = new StringBuilder();
+                process.ErrorDataReceived += (sender, e) =>
+                {
+                    if (!string.IsNullOrEmpty(e.Data))
+                    {
+                        error.AppendLine(e.Data);
+                    }
+                };
+
+                process.BeginErrorReadLine();
+                process.WaitForExit();
+
+                if (process.ExitCode != 0)
+                {
+                    Console.WriteLine($"Error converting {inputAudioFile}:");
+                    Console.WriteLine(error.ToString());
+                }
+            }
+
+            string outputFile = Path.Combine(directory_SESX, Path.GetFileNameWithoutExtension(primaryFile) + ".sesx");
+            Console.WriteLine("SESX created at " + outputFile);
             SESX.Save(outputFile);
 
         }
