8f29b94647ee3f2292a6d7164d6d850e95df7921
123connorpoop <rct3fan55@yahoo.com>
2024-06-04 21:45:11 -0400

    Add jam support in RCompCreate

--- Scripts/RCompCreate.cs
+++ Scripts/RCompCreate.cs
@@ -4,6 +4,8 @@ class RCompCreate
     const string USAGE = "Usage: RCompCreate <input_file>";
     const string RCOMP_SYMBOL_NAME = "RCOMP►CHARACTERS";
     const string RCOMP_SHADING_NAME = "RCOMP►SHADING";
+    const string RCOMP_JAM_MASK_NAME = "RCOMP►JamMask";
+    const string RCOMP_JAM_CHARS_NAME = "RCOMP►JamChars";
     public static void Run(string[] args)
     {
         if (args.Length != 1)
@@ -64,8 +66,56 @@ class RCompCreate
                 continue;
             }
             scene.Layers[RCompShadingLayerIndex[0]].Name = RCOMP_SHADING_NAME;
-            // find the RCOMP►SHADING layer put it right above the RCOMP►CHARACTERS layer
-            scene.ReorderLayer(RCompShadingLayerIndex[0], RCompLayerIndex[0]);
+            (scene.Layers[RCompShadingLayerIndex[0]].KeyFrames[0].Elements[0] as SymbolInstance)!.BlendMode = "overlay";
+            inp = null;
+            do
+            {
+                System.Console.WriteLine("Composite Jam Layer? (y/n)");
+                inp = System.Console.ReadLine();
+                if (inp == null) continue;
+                if (inp.ToLower().StartsWith('y'))
+                {
+                    break;
+                }
+            } while (inp is null);
+            if (inp.ToLower().StartsWith('y'))
+            {
+                var jamMaskLayerIndex = scene.FindLayerIndex("JAM_MASK");
+                if (jamMaskLayerIndex.Count != 1)
+                {
+                    System.Console.WriteLine("JAM_MASK layer not found in scene OR multiple layers found.");
+                    continue;
+                }
+                var jamMaskLayer = scene.Layers[jamMaskLayerIndex[0]];
+                // convert jam mask layer to symbol
+                string rCompJamMaskSymbolName = RCOMP_JAM_MASK_NAME + sceneNum;
+                scene.ConvertLayersToSymbol(new List<Layer> { jamMaskLayer }, rCompJamMaskSymbolName);
+                jamMaskLayerIndex = scene.FindLayerIndex(rCompJamMaskSymbolName);
+                if (jamMaskLayerIndex.Count != 1)
+                {
+                    throw new Exception("Failed to find jam mask layer after converting it to symbol. Something is wrong.");
+                }
+                jamMaskLayer = scene.Layers[jamMaskLayerIndex[0]];
+                jamMaskLayer.Name = RCOMP_JAM_MASK_NAME;
+
+                (jamMaskLayer.KeyFrames[0].Elements[0] as SymbolInstance)!.BlendMode = "erase";
+                // convert jam mask layer AND rcomp characters layer to symbol
+                string rCompJamCharsName = RCOMP_JAM_CHARS_NAME + sceneNum;
+                scene.ConvertLayersToSymbol(new List<Layer> { jamMaskLayer, RCompLayer }, rCompJamCharsName);
+                // find the newly created layer
+                var jamCharsRCompLayerIndex = scene.FindLayerIndex(rCompJamCharsName);
+                if (jamCharsRCompLayerIndex.Count != 1)
+                {
+                    throw new Exception("Failed to find jam chars layer after converting it to symbol. Something is wrong.");
+                }
+                var jamCharsRCompLayer = scene.Layers[jamCharsRCompLayerIndex[0]];
+                jamCharsRCompLayer.Name = RCOMP_JAM_CHARS_NAME;
+                (jamCharsRCompLayer.KeyFrames[0].Elements[0] as SymbolInstance)!.BlendMode = "layer";
+                // put shading layer above jam chars layer
+                scene.ReorderLayer(RCompShadingLayerIndex[0], jamCharsRCompLayerIndex[0]);
+            }
+            // find the RCOMP►SHADING layer put it right above the RCOMP►CHARACTERS layer OR above the jamCharsRComp Layer
+            else scene.ReorderLayer(RCompShadingLayerIndex[0], RCompLayerIndex[0]);
         } while (true);
         inp = null;
         do
