41710b28dd316a17d0c1fd1b2f2367064d4f0a44
PixelPone897 <spindashmaster99@gmail.com>
2024-09-09 23:15:16 -0400

    Update documentation for better readability

---
+++ SkiaRendering/StyleUtils.cs
@@ -0,0 +1,39 @@
+﻿using SkiaSharp;
+
+namespace SkiaRendering
+{
+    internal class StyleUtils
+    {
+        public static SKStrokeCap GetStrokeCap(string caps)
+        {
+            if (caps == "round")
+            {
+                return SKStrokeCap.Round;
+            }
+            else if (caps == "butt" || caps == "none")
+            {
+                return SKStrokeCap.Square;
+            }
+            else
+            {
+                return SKStrokeCap.Round;
+            }
+        }
+
+        public static SKStrokeJoin GetStrokeJoin(string joints)
+        {
+            if (joints == "round")
+            {
+                return SKStrokeJoin.Round;
+            }
+            else if (joints == "butt")
+            {
+                return SKStrokeJoin.Bevel;
+            }
+            else
+            {
+                return SKStrokeJoin.Miter;
+            }
+        }
+    }
+}
