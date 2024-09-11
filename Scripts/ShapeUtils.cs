84851583b2532d103f14816ded774742cda58a6d
PixelPone897 <spindashmaster99@gmail.com>
2024-09-11 00:38:19 -0400

    Create SVG elements for FillStyle CSXFL object

---
+++ Scripts/ShapeUtils.cs
@@ -0,0 +1,48 @@
+﻿using CsXFL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SkiaRendering
+{
+    /// <summary>
+    /// Parses a XFL DOMShape element into its equivalent SVG path elements.
+    /// </summary>
+    internal class ShapeUtils
+    {
+        /// <summary>
+        /// Convert XFL DOMShape element into its equivalent SVG path elements.
+        /// </summary>
+        /// <param name="shapeElement">The XFL DOMShape being converted.</param>
+        /// <param name="mask">If true, all fill colors will be set to #FFFFFF. This ensures
+        /// that the resulting mask is fully transparent.</param>
+        /// <returns>A 3-tuple consisting of: SVG g element containing filled paths elements,
+        /// SVG g element containing stroked path elements, and
+        /// dict of extra elements to put in SVG defs element (e.g.filters and gradients)</returns>
+        public static (XElement?, XElement?, Dictionary<int, XElement>?) 
+            ConvertShapeToSVG(Shape shapeElement, bool mask = false)
+        {
+            Dictionary<int, Dictionary<string, string>> fillStyleElements = new Dictionary<int, Dictionary<string, string>>();
+            Dictionary<string, XElement> extraDefElements = new Dictionary<string, XElement>();
+
+            foreach(FillStyle fillStyle in shapeElement.Fills)
+            {
+                int index = fillStyle.Index;
+                if(mask)
+                {
+                    Dictionary<string, string> attributes = new Dictionary<string, string>()
+                    {
+                        {"fill", "#FFFFFF" },
+                        {"stroke", "none" }
+                    };
+
+                }
+            }
+
+            return (null, null, null);
+        }
+    }
+}
--- Scripts/StyleUtils.cs
+++ Scripts/StyleUtils.cs
@@ -1,39 +1,50 @@
-﻿using SkiaSharp;
+﻿using CsXFL;
+using Scripts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace SkiaRendering
 {
-    internal class StyleUtils
+    class StyleUtils
     {
-        public static SKStrokeCap GetStrokeCap(string caps)
+        /// <summary>
+        /// Parse CSXFL FillStyle element of its SVG style attributes and any extra-defs.
+        /// </summary>
+        /// <param name="fillStyle">CSXFL FillStyle element whose SVG elements will be parsed from.</param>
+        /// <returns>A tuple: attributes which is a Dictionary of SVG style attributes</returns>
+        /// <exception cref="Exception">When there is a fillStyle that a RadialGradient or one
+        /// is not recognized</exception>
+        public static (Dictionary<string, string>, Dictionary<string, XElement>) ParseFillStyle(FillStyle fillStyle)
         {
-            if (caps == "round")
-            {
-                return SKStrokeCap.Round;
-            }
-            else if (caps == "butt" || caps == "none")
-            {
-                return SKStrokeCap.Square;
-            }
-            else
-            {
-                return SKStrokeCap.Round;
-            }
-        }
+            Dictionary<string, string> attributes = new Dictionary<string, string>();
+            Dictionary<string, XElement> extraDefElements = new Dictionary<string, XElement>();
 
-        public static SKStrokeJoin GetStrokeJoin(string joints)
-        {
-            if (joints == "round")
+            if(fillStyle.SolidColor != null)
             {
-                return SKStrokeJoin.Round;
+                attributes["fill"] = fillStyle.SolidColor.Color;
+                attributes["fill-opacity"] = fillStyle.SolidColor.Alpha.ToString();
             }
-            else if (joints == "butt")
+            else if(fillStyle.LinearGradient != null)
             {
-                return SKStrokeJoin.Bevel;
+                XElement gradientElement = GradientUtils.ConvertLinearGradientToSVG(fillStyle.LinearGradient);
+                attributes["fill"] = $"url(#{gradientElement.Attribute("id")!.Value}";
+                extraDefElements[gradientElement.Attribute("id")!.Value] = gradientElement;
+            }
+            else if(fillStyle.RadialGradient != null)
+            {
+                // TODO: Support RadialGradient
+                throw new Exception("RadialGradient is not supported yet!");
             }
             else
             {
-                return SKStrokeJoin.Miter;
+                throw new Exception($"Unknown fill style: {fillStyle.ToString()}");
             }
+
+            return (attributes, extraDefElements);
         }
     }
 }
