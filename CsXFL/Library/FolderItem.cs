73aab6d5d978b6f2a1ccd79b54626f09dfd45d84
123connorpoop <rct3fan55@yahoo.com>
2025-04-08 14:22:08 -0400

    Set ItemType to "folder" in FolderItem constructor

--- CsXFL/Library/FolderItem.cs
+++ CsXFL/Library/FolderItem.cs
@@ -5,6 +5,7 @@ public class FolderItem : Item {
     internal const string FOLDER_NODE_IDENTIFIER = "DOMFolderItem";
     internal FolderItem(XNamespace ns) : base() { 
         this.ns = ns;
+        ItemType = "folder";
         root = new XElement(ns + FolderItem.FOLDER_NODE_IDENTIFIER);
     }
     internal FolderItem(in XElement folderNode) : base(folderNode, "folder") { }
